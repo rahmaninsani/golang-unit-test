8365d4da70901de31c3a178799b76eff9cefef64
Rahman Insani <rahmaninsani54@gmail.com>
2022-08-28 18:20:47 +0700

    Learn: Unit Test Assertions with Testify

--- helper/hello_world.go
+++ helper/hello_world.go
@@ -2,5 +2,5 @@ package helper
 
 func HelloWorld(name string) string {
 	return "Hello " + name
-	// return "Hi " + name // for error unit tests
+	//return "Hi " + name // for error unit tests
 }
--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -2,8 +2,27 @@ package helper
 
 import (
 	"fmt"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
+
+// assert -> Fail()
+// require -> FailNow()
+
+func TestHelloWorldAssert(t *testing.T) {
+	result := HelloWorld("Charlie")
+	errorMessage := fmt.Sprintf("Expected 'Hello Charlie', but got '%s'", result)
+	assert.Equal(t, "Hello Charlie", result, errorMessage)
+	fmt.Println("TestHelloWorldAssert Done")
+}
+
+func TestHelloWorldRequire(t *testing.T) {
+	result := HelloWorld("Charlie")
+	errorMessage := fmt.Sprintf("Expected 'Hello Charlie', but got '%s'", result)
+	require.Equal(t, "Hello Charlie", result, errorMessage)
+	fmt.Println("TestHelloWorldRequire Done")
+}
 
 func TestHelloWorldAlice(t *testing.T) {
 	result := HelloWorld("Alice")
